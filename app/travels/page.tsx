6ebaf4902f99f79b16f2a7a88dcf18756feb4423
Nahuel Luca <nahuelluca20@gmail.com>
2023-10-02 16:22:21 -0300

    set to use server

--- app/travels/page.tsx
+++ app/travels/page.tsx
@@ -1,39 +1,45 @@
-"use client";
+// "use client";
 import Link from "next/link";
-import {useEffect, useState} from "react";
+import {Suspense} from "react";
 
 import TravelCard from "@/components/travel-card";
 
 import {getData} from "../actions";
 
-export default function Page() {
-  const [travels, setTravels] = useState<any>();
+import Loading from "./loading";
+
+export default async function Page() {
+  // const [travels, setTravels] = useState<any>();
 
   async function getTravels() {
     const data = await getData();
 
-    await setTravels(data);
+    // await setTravels(data);
+    return data;
   }
 
-  useEffect(() => {
-    getTravels();
-  }, []);
+  const travels = await getTravels();
+  // useEffect(() => {
+  //   getTravels();
+  // }, []);
 
   return (
     <main className="max-w-[800px] px-5 lg:px-0 w-full justify-center text-center mx-auto pt-2 pb-10">
       <h1 className="text-5xl w-full mt-12 text-white">Travels Feed</h1>
       <span className="text-lg w-full py-2 text-gray-400">Feed for your favorites travels</span>
       <section className="mt-10 grid gap-6">
-        {travels &&
-          travels?.props?.map((post: any) => (
-            <Link key={post.id} href={`/travels/${post.id}`}>
-              <TravelCard
-                author={post?.author?.name ?? ""}
-                content={post?.content ?? ""}
-                title={post?.title ?? ""}
-              />
-            </Link>
-          ))}
+        <Suspense fallback={<Loading />}>
+          {travels &&
+            travels?.props?.map((post: any) => (
+              <Link key={post.id} href={`/travels/${post.id}`}>
+                <TravelCard
+                  author={post?.author?.name ?? ""}
+                  content={post?.content ?? ""}
+                  title={post?.title ?? ""}
+                />
+              </Link>
+            ))}
+        </Suspense>
       </section>
     </main>
   );
--- components/travel-card.tsx
+++ components/travel-card.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import {Suspense} from "react";
 
 import PaisajeImg from "@/assets/paisaje.jpg";
 
@@ -12,19 +13,21 @@ export default function TravelCard({
   author: string;
 }) {
   return (
-    <div className="grid md:flex text-start md:gap-8 shadow-lg rounded-md w-full bg-slate-500 text-white overflow-hidden">
-      <Image
-        alt="mendoza"
-        className="w-full md:w-[200px]"
-        height={200}
-        src={PaisajeImg}
-        width={200}
-      />
-      <div className="grid items-center px-2 py-2 md:py-0">
-        <h1>{title ?? ""}</h1>
-        <p>{content ?? ""}</p>
-        <span>{author ?? ""}</span>
+    <Suspense fallback={<span>Cargando... {title}</span>}>
+      <div className="grid md:flex text-start md:gap-8 shadow-lg rounded-md w-full bg-slate-500 text-white overflow-hidden">
+        <Image
+          alt="mendoza"
+          className="w-full md:w-[200px]"
+          height={200}
+          src={PaisajeImg}
+          width={200}
+        />
+        <div className="grid items-center px-2 py-2 md:py-0">
+          <h1>{title ?? ""}</h1>
+          <p>{content ?? ""}</p>
+          <span>{author ?? ""}</span>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
