e3d4ca88ff4239ee8dd367446155dd39388ce600
Nahuel Luca <nahuelluca20@gmail.com>
2023-10-02 12:57:02 -0300

    remove: get data

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,6 @@
 import AddForm from "@/components/add-form";
 
-import prisma from "../lib/prisma";
-
-async function getData() {
-  const post = await prisma.post.findMany({
-    include: {
-      author: {
-        select: {name: true},
-      },
-    },
-  });
-
-  return {
-    props: post,
-  };
-}
-
 export default async function Home() {
-  // const data = await getData();
-
   return (
     <main className="max-w-[800px] w-full justify-center text-center mx-auto py-2">
       <h1 className="text-5xl w-full mt-12 text-white">Post Travels</h1>
