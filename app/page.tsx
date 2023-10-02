4ffa961b19cfde93300a9af283a77d523d0eeb09
Nahuel Luca <nahuelluca20@gmail.com>
2023-10-02 13:05:49 -0300

    move travel cards to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,43 @@
+import Link from "next/link";
+
 import AddForm from "@/components/add-form";
+import TravelCard from "@/components/travel-card";
+import prisma from "@/lib/prisma";
+async function getData() {
+  const post = await prisma.post.findMany({
+    include: {
+      author: {
+        select: {name: true},
+      },
+    },
+  });
+
+  return {
+    props: post,
+  };
+}
 
 export default async function Home() {
+  const data = await getData();
+
   return (
-    <main className="max-w-[800px] w-full justify-center text-center mx-auto py-2">
+    <main className="max-w-[800px] w-full justify-center text-center mx-auto pt-2 pb-10">
       <h1 className="text-5xl w-full mt-12 text-white">Post Travels</h1>
       <span className="text-lg w-full py-2 text-gray-400">Post here your favorites travels</span>
       <section className="mt-10">
         <AddForm />
+        <section className="mt-10 grid gap-6">
+          {data &&
+            data?.props.map((post) => (
+              <Link key={post.id} href={`/travels/${post.id}`}>
+                <TravelCard
+                  author={post?.author?.name ?? ""}
+                  content={post?.content ?? ""}
+                  title={post?.title ?? ""}
+                />
+              </Link>
+            ))}
+        </section>
       </section>
     </main>
   );
